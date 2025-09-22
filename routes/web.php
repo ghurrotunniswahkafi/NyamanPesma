531d839e42725179f406ba502e5c269dd6921a9c
rsarmn <reisyarahma27@gmail.com>
2025-09-22 19:22:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-<<<<<<< HEAD
-    return view('welcome');
+return view('welcome');
 });
-=======
-    return view('home');
-});
+
+return view('home');
 
 Route::get('/facility/asrama', function () {
     return view('facility.asrama');
@@ -31,4 +29,3 @@ Route::get('facility/asrama/', [AsramaController::class, 'show'])->name('asrama.
 Route::get('/facility/asrama', [AsramaController::class, 'index'])->name('asrama.index');
  */
 Route::get('/facility/asrama', [AsramaController::class, 'index']);
->>>>>>> ca1011c (Home-Asrama)
