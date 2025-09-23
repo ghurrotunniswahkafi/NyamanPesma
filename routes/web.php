b6e4f1f6cc9017e41bf80e024e01b2132cfce2b4
ghurrotun niswah kafi <ginuniswah@gmail.com>
2025-09-23 13:29:35 +0700

    Update web.php
    nambah admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FasilitasController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// The root route for the dashboard
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+// The main resource route for all CRUD operations on 'fasilitas'
+// This single line handles index, create, store, edit, update, and destroy
+Route::resource('fasilitas', FasilitasController::class);
+
+// Other resource routes
+Route::resource('asrama', AsramaController::class);
+Route::resource('users', UserController::class);
+Route::resource('reservasi', ReservasiController::class);
+Route::resource('pembayaran', PembayaranController::class);
+Route::resource('laporan', LaporanController::class);
+Route::resource('profile', ProfileController::class);
+Route::resource('settings', SettingsController::class);
+Route::resource('roles', RoleController::class);
